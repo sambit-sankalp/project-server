739a51385b75a12811e13f4c89337e53a5dc62b1
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2022-12-22 01:47:53 +0530

    fix:small fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import gameRoutes from './routes/gameRoutes.js';
@@ -18,8 +18,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/user', userRoutes);
-app.use('/api/game', gameRoutes)
-
+app.use('/api/game', gameRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
@@ -29,6 +28,6 @@ app.use(notFound);
 
 app.use(errorHandler);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
